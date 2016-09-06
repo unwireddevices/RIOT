86f2960168830535bfe6623567e3c118d0e501eb
Morsview <mikhail.churikov@gmail.com>
2016-09-06 15:06:33 +0300

    [STM32L1][LPM] Fixing LPM_SLEEP.
    It is nessesary before WFI disable interrupts and execute data memory
    barier. After WFI NOP and enablibg interrupts is needed.

--- core/kernel_init.c
+++ core/kernel_init.c
@@ -71,7 +71,7 @@ static void *idle_thread(void *arg)
         }
         else {
             //lpm_set(LPM_IDLE);
-            //lpm_set(LPM_SLEEP);
+            lpm_set(LPM_SLEEP);
             /* lpm_set(LPM_POWERDOWN); */
         }
     }

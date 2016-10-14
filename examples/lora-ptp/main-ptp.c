d697dfdbc77bfc60449ddd754bed28b0b13377b0
Cr0s <Cr0s@qip.ru>
2016-10-14 19:20:01 +0300

    main-ptp.c: fixed DR setup checking possibly leads to invalid DR

--- examples/lora-ptp/main-ptp.c
+++ examples/lora-ptp/main-ptp.c
@@ -349,7 +349,7 @@ static int cmd_set(int argc, char **argv)
     if (argc != 3) {
         puts("usage: get <key> <value>");
         puts("keys:");
-        puts("\tdr <0-7> -- sets device data rate [0 - slowest, 3 - average, 7 - fastest]");
+        puts("\tdr <0-6> -- sets device data rate [0 - slowest, 3 - average, 6 - fastest]");
         puts("\tfreq <frequency> -- sets LoRa frequency [Hz]");
         puts("\tport <0-255> -- sets port to transmit and receive data");
 
@@ -366,8 +366,8 @@ static int cmd_set(int argc, char **argv)
     if (strcmp(key, "dr") == 0) {
         uint8_t v = strtol(value, NULL, 10);
 
-        if (v > 7) {
-            puts("set dr: datarate value must be from 0 to 7");
+        if (v > 6) {
+            puts("set dr: datarate value must be from 0 to 6");
             return 1;
         }
 

61a9a333061ac7aa81066eab2c7d67ae1fb2ee8b
Cr0s <Cr0s@qip.ru>
2016-11-11 18:40:04 +0300

    sht21.c: cleanup

--- drivers/sht21/sht21.c
+++ drivers/sht21/sht21.c
@@ -26,51 +26,6 @@
 extern "C" {
 #endif
 
-/*---------------------------------------------------------------------------*/
-/**
- *  * \name SHT21 register addresses and values
- *   * @{
- *    */
-#define SHT21_USER_REG_READ             (0xE7)
-#define SHT21_USER_REG_WRITE            (0xE6)
-#define SHT21_USER_REG_RESERVED_BITS    (0x38)
-
-#define SHT21_TEMPERATURE_HM_CMD        (0xE3)
-#define SHT21_HUMIDITY_HM_CMD           (0xE5)
-#define SHT21_TEMPERATURE_NHM_CMD       (0xF3)
-#define SHT21_HUMIDITY_NHM_CMD          (0xF5)
-#define SHT21_RESET_CMD                 (0xFE)
-
-#define SHT21_STATUS_MASK               (0xFC)
-
-#define SHT21_RESOLUTION_12b_14b        ((0 << 7) | (0 << 0))
-#define SHT21_RESOLUTION_8b_12b         ((0 << 7) | (1 << 0))
-#define SHT21_RESOLUTION_10b_13b        ((1 << 7) | (0 << 0))
-#define SHT21_RESOLUTION_11b_11b        ((1 << 7) | (1 << 0))
-#define SHT21_BATTERY_ABOVE_2V25        (0 << 6)
-#define SHT21_BATTERY_BELOW_2V25        (1 << 6)
-#define SHT21_ONCHIP_HEATER_ENABLE      (1 << 2)
-#define SHT21_ONCHIP_HEATER_DISABLE     (0 << 2)
-#define SHT21_OTP_RELOAD_ENABLE         (0 << 1)
-#define SHT21_OTP_RELOAD_DISABLE        (1 << 1)
-/** @} */
-/*---------------------------------------------------------------------------*/
-/**
- *  * \name SHT21 configuration values
- *   * @{
- *    */
-#define SHT21_DEFAULT_CONFIG            (SHT21_RESOLUTION_12b_14b | \
-                                             SHT21_ONCHIP_HEATER_DISABLE | \
-                                             SHT21_BATTERY_ABOVE_2V25 | \
-                                             SHT21_OTP_RELOAD_DISABLE)
-
-#define SHT21_USER_CONFIG               (SHT21_RESOLUTION_8b_12b | \
-                                             SHT21_ONCHIP_HEATER_DISABLE | \
-                                             SHT21_BATTERY_ABOVE_2V25 | \
-                                             SHT21_OTP_RELOAD_DISABLE)
-/** @} */
-/*---------------------------------------------------------------------------*/
-
 /**
  * @brief SHT21 driver initialization routine
  * @note Corresponding I2C peripheral MUST be initialized before
@@ -93,7 +48,7 @@ int sht21_init(sht21_t * dev)
     i2c_init_master(dev->i2c, I2C_SPEED_NORMAL);
 
     if (i2c_read_reg(dev->i2c, SHT21_ADDRESS, SHT21_USER_REG_READ, (char *)&config) != 1) {
-      puts("[sht21 driver] Error: sensor not found.\n");
+      puts("[sht21 driver] Error: sensor not found");
       i2c_release(dev->i2c);
 
       return -1;
@@ -105,7 +60,7 @@ int sht21_init(sht21_t * dev)
     /* Set configuration bits without changing those reserved */
     config |= SHT21_USER_CONFIG;
 
-    puts("[sht21 driver] Setting configuration register.\n");
+    puts("[sht21 driver] Setting configuration register");
     i2c_write_reg(dev->i2c, SHT21_ADDRESS, SHT21_USER_REG_WRITE, config);
     i2c_release(dev->i2c);
 

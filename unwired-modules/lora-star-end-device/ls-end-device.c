39e137778b8124ab513b95093ca79b343b877c96
Cr0s <Cr0s@qip.ru>
2016-11-29 16:25:27 +0300

    ls-end-device.c: copy-paste fix

--- unwired-modules/lora-star-end-device/ls-end-device.c
+++ unwired-modules/lora-star-end-device/ls-end-device.c
@@ -288,7 +288,7 @@ static bool frame_recv(ls_ed_t *ls, ls_frame_t *frame)
 
 			/* quasi-random delay up to 8.4 seconds for collision avoidance */
 			unsigned int delay = ((xtimer_now() & 0xFF) << 15);
-			printf("ls-ed: quasi-random retransmission delay %d msec\n", (unsigned int) (delay/1e3));
+			printf("ls-ed: quasi-random delay %d msec\n", (unsigned int) (delay/1e3));
 			xtimer_usleep(delay);
 
             /* Proceed to join procedure as requested */

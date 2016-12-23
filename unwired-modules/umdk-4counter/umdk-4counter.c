25dbb50dd01d75732bc1bad72e586cb3b9bd70a1
SHEFF <sheff@sheff.com>
2016-12-23 16:41:13 +0300

    umdk-4counter: Realization without IRQ. Do not work cmd

--- unwired-modules/umdk-4counter/umdk-4counter.c
+++ unwired-modules/umdk-4counter/umdk-4counter.c
@@ -38,21 +38,122 @@ extern "C" {
 #include "xtimer.h"
 #include "rtc-timers.h"
 
+
+static uint8_t time_detect = (uint8_t)(UMDK_4COUNT_DEBOUNCE_TIME_MS / UMDK_4COUNT_DETECT_COUNT);
+
 static kernel_pid_t handler_pid;
-static uint32_t last_pressed[4] = {
-};
 
 static uwnds_cb_t *callback;
-static rtctimer_t timer;
+static rtctimer_t counter_timer;
+static rtctimer_t publishing_timer;
 
-static msg_t handler_msg = {};
+static msg_t counter_msg = {.type = 0};
+static msg_t publishing_msg = {.type = 1};
+
 
 static struct  {
     uint8_t is_valid;
-    uint32_t count_value[4];
+    uint32_t count_value[UMDK_4COUNT_NUM_SENS];
     uint8_t publish_period;
 } conf_counter;
 
+static gpio_t pins_sens[UMDK_4COUNT_NUM_SENS] = {UMDK_4COUNT_1, UMDK_4COUNT_2, UMDK_4COUNT_3, UMDK_4COUNT_4};
+
+
+//static void umdk_4count_gpio_mode(gpio_t pin, gpio_mode_t mode, umdk_4counter_signal_t signal  )
+//{
+//  GPIO_TypeDef *port = (GPIO_TypeDef *)(pin & ~(0x0f));
+//
+//  int pin_num =  (pin & 0x0f);
+//
+//  umdk_4counter_signal_t sign = signal;
+//
+//  switch(sign)
+//    {
+//      case DIGITAL:
+//
+//	/* set mode */
+//	port->MODER &= ~(0x3 << (2 * pin_num));
+//	port->MODER |=  ((mode & 0x3) << (2 * pin_num));
+//
+//	/* set pull resistor configuration */
+//	port->PUPDR &= ~(0x3 << (2 * pin_num));
+//	port->PUPDR |=  (((mode >> 2) & 0x3) << (2 * pin_num));
+//	break;
+//
+//      case ANALOG:
+//
+//	port->MODER &= ~(0x3 << (2 * pin_num));
+//	port->MODER |= (0x3 << (2 * pin_num));
+//	break;
+//
+//      default:
+//	break;
+//    }
+//}
+
+static void umdk_4count_counter_int(void)
+{
+
+    int8_t now_value[UMDK_4COUNT_NUM_SENS] = {0};
+    int8_t last_value[UMDK_4COUNT_NUM_SENS] = {0};
+    bool accept_value[UMDK_4COUNT_NUM_SENS] = {false};
+
+    /* Prevent low power mode */
+    lpm_prevent_sleep = 1;
+
+   /* for(int i=0;i<UMDK_4COUNT_NUM_SENS;i++)
+      {
+	umdk_4count_gpio_mode(pins_sens[i], GPIO_IN_PU, DIGITAL);
+      }*/
+
+
+    for(int8_t i=0;i<UMDK_4COUNT_DETECT_COUNT;i++)
+      {
+	for(int j=0;j<UMDK_4COUNT_NUM_SENS;j++)
+	  {
+
+	    now_value[j] = !gpio_read(pins_sens[j]);
+
+	    if(now_value[j] == 0)
+	      {
+		printf("[umdk-4counter] Counting %d rejected	%d/%d\n", j+1, i+1, UMDK_4COUNT_DETECT_COUNT);
+		accept_value[j] = false;
+	      }
+	    else if(last_value[j] == 1)
+	      {
+		printf("[umdk-4counter] Counting %d accept	%d/%d\n", j+1, i+1, UMDK_4COUNT_DETECT_COUNT);
+		accept_value[j] = true;
+	      }
+
+	    last_value[j] = now_value[j];
+	  }
+	printf("\n");
+	xtimer_usleep(time_detect * 1000);
+      }
+
+
+    /* Increase pulses count for current input */
+    for(int i=0;i<UMDK_4COUNT_NUM_SENS;i++)
+      {
+      if(accept_value[i] == true)
+	{
+	  conf_counter.count_value[i]++;
+	  printf("[umdk-4counter] Count %d   Value %d\n", i+1, (int)conf_counter.count_value[i]);
+	  accept_value[i] = false;
+	}
+      }
+   /* for(int i=0;i<UMDK_4COUNT_NUM_SENS;i++)
+      {
+	umdk_4count_gpio_mode(pins_sens[i], GPIO_IN_PU, ANALOG);
+      }*/
+
+    /* Restart counting timer */
+    rtctimers_set_msg(&counter_timer, UMDK_4COUNT_SLEEP_TIME_SEC , &counter_msg, handler_pid);
+
+    /* Allow sleep */
+    lpm_prevent_sleep = 0;
+}
 
 static inline void save_config(void)
 {
@@ -63,75 +164,60 @@ static inline void save_config(void)
 static void *handler(void *arg)
 {
     msg_t msg;
-    msg_t msg_queue[2];
+    msg_t msg_queue[4];
 
-    msg_init_queue(msg_queue, 2);
+    msg_init_queue(msg_queue, 4);
 
     while (1) {
         msg_receive(&msg);
 
-        lpm_prevent_sleep = 1;
+        umdk_4counter_msg_t type = msg.type;
 
-        module_data_t data;
-        data.length = 1 + 4 * 4;
+        switch(type)
+        {
+          case COUNTING:
+		umdk_4count_counter_int();
+            break;
 
-        /* Write module ID */
-        data.data[0] = UNWDS_4COUNTER_MODULE_ID;
+          case PUBLISHING:
+	        lpm_prevent_sleep = 1;
 
-        /* Write four counter values */
-        uint32_t *tmp = (uint32_t *)(&data.data[1]);
+	        module_data_t data;
+	        data.length = 1 + 4 * UMDK_4COUNT_NUM_SENS;
 
-        *(tmp + 0)  = conf_counter.count_value[0];
-        *(tmp + 1)  = conf_counter.count_value[1];
-        *(tmp + 2)  = conf_counter.count_value[2];
-        *(tmp + 3)  = conf_counter.count_value[3];
+	        /* Write module ID */
+	        data.data[0] = UNWDS_4COUNTER_MODULE_ID;
 
-        save_config(); /* Save values into NVRAM */
+	        /* Write four counter values */
+	        uint32_t *tmp = (uint32_t *)(&data.data[1]);
 
-        callback(&data);
+	        *(tmp + 0)  = conf_counter.count_value[0];
+	        *(tmp + 1)  = conf_counter.count_value[1];
+	        *(tmp + 2)  = conf_counter.count_value[2];
+	        *(tmp + 3)  = conf_counter.count_value[3];
 
-        /* Restart timer */
-        if (conf_counter.publish_period) {
-            rtctimers_set_msg(&timer, 3600 * conf_counter.publish_period, &handler_msg, handler_pid);
+	        save_config(); /* Save values into NVRAM */
+
+	        callback(&data);
+
+	        /* Restart timer */
+	        if (conf_counter.publish_period) {
+	            rtctimers_set_msg(&publishing_timer, UMDK_4COUNT_VALUE_PERIOD_SEC * conf_counter.publish_period, &publishing_msg, handler_pid);
+	        }
+
+	        /* Allow sleep */
+	        lpm_prevent_sleep = 0;
+	  break;
+
+	  default:
+	      break;
         }
-
-        /* Allow sleep */
-        lpm_prevent_sleep = 0;
     }
 
     return NULL;
 }
 
-static void counter_int(void *arg)
-{
-    uint8_t input_num = ((int) arg) - 1;
 
-    /* Prevent low power mode */
-    lpm_prevent_sleep = 1;
-
-    uint32_t now = xtimer_now();
-
-    /* Timer overflows every ~71 minutes */
-    uint32_t overflow = 0;
-    if (last_pressed[input_num] > now) {
-        overflow = UINT32_MAX - last_pressed[input_num];
-    }
-
-    /* Don't accept a pulse if it did occur earlier than last pulse time plus debouncing time */
-    if (overflow + now - last_pressed[input_num] <= UMDK_4COUNT_DEBOUNCE_TIME_MS * 1000) {
-        printf("[4counter] Counting %d rejected\n", (input_num + 1));
-        return;
-    }
-
-    /* Increase pulses count for current input */
-    conf_counter.count_value[input_num]++;
-
-    /* Remember time of current pulse */
-    last_pressed[input_num] = now;
-
-    /* Allow sleep */
-    lpm_prevent_sleep = 0;
-}
 
 void umdk_4counter_init(uint32_t *non_gpio_pin_map, uwnds_cb_t *event_callback)
 {
@@ -141,11 +227,10 @@ void umdk_4counter_init(uint32_t *non_gpio_pin_map, uwnds_cb_t *event_callback)
 
     callback = event_callback;
 
-    /* Initialize input interrupts */
-    gpio_init_int(UMDK_4COUNT_1, GPIO_IN_PU, GPIO_FALLING, counter_int, (void *) 1);
-    gpio_init_int(UMDK_4COUNT_2, GPIO_IN_PU, GPIO_FALLING, counter_int, (void *) 2);
-    gpio_init_int(UMDK_4COUNT_3, GPIO_IN_PU, GPIO_FALLING, counter_int, (void *) 3);
-    gpio_init_int(UMDK_4COUNT_4, GPIO_IN_PU, GPIO_FALLING, counter_int, (void *) 4);
+    for(int i=0;i<UMDK_4COUNT_NUM_SENS;i++)
+      {
+	gpio_init(pins_sens[i], GPIO_IN_PU);
+      }
 
     /* Create handler thread */
     char *stack = (char *) allocate_stack();
@@ -157,12 +242,15 @@ void umdk_4counter_init(uint32_t *non_gpio_pin_map, uwnds_cb_t *event_callback)
     /* Load config from NVRAM */
     unwds_read_nvram_config(UNWDS_4COUNTER_MODULE_ID, (uint8_t *) &conf_counter, sizeof(conf_counter));
 
-    printf("[4counter] Current publish period: %d hour(s)\n", conf_counter.publish_period);
+    printf("[umdk-4counter] Current publish period: %d hour(s)\n", conf_counter.publish_period);
 
     handler_pid = thread_create(stack, UNWDS_STACK_SIZE_BYTES, THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST, handler, NULL, "4counter thread");
 
     /* Start publishing timer */
-    rtctimers_set_msg(&timer, 3600 * conf_counter.publish_period, &handler_msg, handler_pid);
+    rtctimers_set_msg(&publishing_timer, UMDK_4COUNT_VALUE_PERIOD_SEC * conf_counter.publish_period, &publishing_msg, handler_pid);
+
+    /* Start counting timer */
+    rtctimers_set_msg(&counter_timer,UMDK_4COUNT_SLEEP_TIME_SEC , &counter_msg, handler_pid);
 }
 
 
@@ -180,18 +268,18 @@ bool umdk_4counter_cmd(module_data_t *cmd, module_data_t *reply)
             }
 
             uint8_t period = cmd->data[1];
-            rtctimers_remove(&timer);
+            rtctimers_remove(&publishing_timer);
 
             conf_counter.publish_period = period;
             save_config();
 
             /* Don't restart timer if new period is zero */
             if ((conf_counter.publish_period) && (conf_counter.publish_period < (UMDK_4COUNT_PUBLISH_PERIOD_MAX + 1))) {
-                rtctimers_set_msg(&timer, 3600 * conf_counter.publish_period, &handler_msg, handler_pid);
-                printf("[4counter] Period set to %d hour (s)\n", conf_counter.publish_period);
+                rtctimers_set_msg(&publishing_timer, UMDK_4COUNT_VALUE_PERIOD_SEC * conf_counter.publish_period, &publishing_msg, handler_pid);
+                printf("[umdk-4counter] Period set to %d hour (s)\n", conf_counter.publish_period);
             }
             else {
-                puts("[4counter] Timer stopped");
+                puts("[umdk-4counter] Timer stopped");
             }
 
             reply->length = 4;
@@ -204,8 +292,9 @@ bool umdk_4counter_cmd(module_data_t *cmd, module_data_t *reply)
         }
 
         case UMDK_4COUNTER_CMD_POLL:
+
             /* Send values to publisher thread */
-            msg_send(&handler_msg, handler_pid);
+            msg_send(&publishing_msg, handler_pid);
 
             return false; /* Don't reply */
 

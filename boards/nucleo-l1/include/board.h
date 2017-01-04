c73ae9498c5682390f9c22f193dd3e7794ec1416
Oleg Artamonov <oleg@unwds.com>
2017-01-04 19:31:50 +0300

    boards/nucleo-l1: add STDIO UART basic configuration
    Needed for LPM

--- boards/nucleo-l1/include/board.h
+++ boards/nucleo-l1/include/board.h
@@ -37,6 +37,14 @@ extern "C" {
 #define XTIMER_BACKOFF      (3)
 /** @} */
 
+/**
+ * @name default STDIO UART port configuration
+ * @{
+ */
+#define UART_STDIO_DEV      (UART_DEV(0))
+#define UART_STDIO_BAUDRATE (115200U)
+/** @} */
+
 #ifdef __cplusplus
 }
 #endif

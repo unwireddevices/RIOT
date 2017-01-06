7a794f23bde8ed07d642ac62e22297f02354e030
Oleg Artamonov <oleg@unwds.com>
2017-01-06 12:20:04 +0300

    board_periph.h: make use of periph_clk_en

--- boards/unwd-range-l1-r2/include/periph_conf.h
+++ boards/unwd-range-l1-r2/include/periph_conf.h
@@ -93,8 +93,8 @@ static const timer_conf_t timer_config[] = {
 
 /* UART 0 (USART1) device configuration */
 #define UART_0_DEV          USART1
-#define UART_0_CLKEN()      (RCC->APB2ENR |= RCC_APB2ENR_USART1EN)
-#define UART_0_CLKDIS()		(RCC->APB2ENR &= ~RCC_APB2ENR_USART1EN)
+#define UART_0_CLKEN()      (periph_clk_en(APB2, RCC_APB2ENR_USART1EN))
+#define UART_0_CLKDIS()		(periph_clk_dis(APB2, RCC_APB2ENR_USART1EN))
 #define UART_0_ISON()		(RCC->APB2ENR & RCC_APB2ENR_USART1EN)
 #define UART_0_CLK          (CLOCK_CORECLOCK)   /* UART clock runs with 32MHz (F_CPU / 1) */
 #define UART_0_IRQ          USART1_IRQn
@@ -107,8 +107,8 @@ static const timer_conf_t timer_config[] = {
 
 /* UART 1 (USART2) device configuration */
 #define UART_1_DEV          USART2
-#define UART_1_CLKEN()      (RCC->APB1ENR |= RCC_APB1ENR_USART2EN)
-#define UART_1_CLKDIS()     (RCC->APB1ENR &= ~RCC_APB1ENR_USART2EN)
+#define UART_1_CLKEN()      (periph_clk_en(APB1, RCC_APB1ENR_USART2EN))
+#define UART_1_CLKDIS()     (periph_clk_dis(APB1, RCC_APB1ENR_USART2EN))
 #define UART_1_ISON()		(RCC->APB1ENR & RCC_APB1ENR_USART2EN)
 #define UART_1_CLK          (CLOCK_CORECLOCK)   /* UART clock runs with 32MHz (F_CPU / 1) */
 #define UART_1_IRQ          USART2_IRQn
@@ -121,8 +121,8 @@ static const timer_conf_t timer_config[] = {
 
 /* UART 2 (USART3) device configuration */
 #define UART_2_DEV          USART3
-#define UART_2_CLKEN()      (RCC->APB1ENR |= RCC_APB1ENR_USART3EN)
-#define UART_2_CLKDIS()     (RCC->APB1ENR &= ~RCC_APB1ENR_USART3EN)
+#define UART_2_CLKEN()      (periph_clk_en(APB1, RCC_APB1ENR_USART3EN))
+#define UART_2_CLKDIS()     (periph_clk_dis(APB1, RCC_APB1ENR_USART3EN))
 #define UART_2_ISON()		(RCC->APB1ENR & RCC_APB1ENR_USART3EN)
 #define UART_2_CLK          (CLOCK_CORECLOCK)   /* UART clock runs with 32MHz (F_CPU / 1) */
 #define UART_2_IRQ          USART3_IRQn
@@ -175,97 +175,97 @@ static const timer_conf_t timer_config[] = {
 /* GPIO channel 0 config */
 #define GPIO_0_PORT         GPIOA                   /* Used for user button 1 */
 #define GPIO_0_PIN          3
-#define GPIO_0_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define GPIO_0_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define GPIO_0_EXTI_CFG()   (SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI3_PA)
 #define GPIO_0_IRQ          EXTI3_IRQn
 /* GPIO channel 1 config */
 #define GPIO_1_PORT         GPIOC
 #define GPIO_1_PIN          7
-#define GPIO_1_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOCEN)
+#define GPIO_1_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOCEN))
 #define GPIO_1_EXTI_CFG()   (SYSCFG->EXTICR[1] |= SYSCFG_EXTICR2_EXTI7_PC)
 #define GPIO_1_IRQ          EXTI9_5_IRQn
 /* GPIO channel 2 config */
 #define GPIO_2_PORT         GPIOA
 #define GPIO_2_PIN          9
-#define GPIO_2_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define GPIO_2_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define GPIO_2_EXTI_CFG()   (SYSCFG->EXTICR[2] |= SYSCFG_EXTICR3_EXTI9_PA)
 #define GPIO_2_IRQ          EXTI9_5_IRQn
 /* GPIO channel 3 config */
 #define GPIO_3_PORT         GPIOA
 #define GPIO_3_PIN          8
-#define GPIO_3_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define GPIO_3_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define GPIO_3_EXTI_CFG()   (SYSCFG->EXTICR[2] |= SYSCFG_EXTICR3_EXTI8_PA)
 #define GPIO_3_IRQ          EXTI9_5_IRQn
 /* GPIO channel 4 config */
 #define GPIO_4_PORT         GPIOB
 #define GPIO_4_PIN          10
-#define GPIO_4_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOBEN)
+#define GPIO_4_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOBEN))
 #define GPIO_4_EXTI_CFG()   (SYSCFG->EXTICR[2] |= SYSCFG_EXTICR3_EXTI10_PB)
 #define GPIO_4_IRQ          EXTI15_10_IRQn
 /* GPIO channel 5 config */
 #define GPIO_5_PORT         GPIOB
 #define GPIO_5_PIN          4
-#define GPIO_5_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOBEN)
+#define GPIO_5_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOBEN))
 #define GPIO_5_EXTI_CFG()   (SYSCFG->EXTICR[1] |= SYSCFG_EXTICR2_EXTI4_PB)
 #define GPIO_5_IRQ          EXTI4_IRQn
 /* GPIO channel 6 config */
 #define GPIO_6_PORT         GPIOC
 #define GPIO_6_PIN          11
-#define GPIO_6_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOCEN)
+#define GPIO_6_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOCEN))
 #define GPIO_6_EXTI_CFG()   (SYSCFG->EXTICR[2] |= SYSCFG_EXTICR3_EXTI11_PC)
 #define GPIO_6_IRQ          EXTI15_10_IRQn
 /* GPIO channel 7 config */
 #define GPIO_7_PORT         GPIOC
 #define GPIO_7_PIN          2
-#define GPIO_7_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOCEN)
+#define GPIO_7_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOCEN))
 #define GPIO_7_EXTI_CFG()   (SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI2_PC)
 #define GPIO_7_IRQ          EXTI2_IRQn
 /* GPIO channel 8 config */
 #define GPIO_8_PORT         GPIOA
 #define GPIO_8_PIN          13
-#define GPIO_8_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define GPIO_8_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define GPIO_8_EXTI_CFG()   (SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI13_PA)
 #define GPIO_8_IRQ          EXTI15_10_IRQn
 /* GPIO channel 9 config */
 #define GPIO_9_PORT         GPIOA
 #define GPIO_9_PIN          14
-#define GPIO_9_CLKEN()      (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define GPIO_9_CLKEN()      (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define GPIO_9_EXTI_CFG()   (SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI14_PA)
 #define GPIO_9_IRQ          EXTI15_10_IRQn
 /* GPIO channel 10 config */
 #define GPIO_10_PORT        GPIOA
 #define GPIO_10_PIN         15
-#define GPIO_10_CLKEN()     (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define GPIO_10_CLKEN()     (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define GPIO_10_EXTI_CFG()  (SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI15_PA)
 #define GPIO_10_IRQ         EXTI15_10_IRQn
 /* GPIO channel 11 config */
 #define GPIO_11_PORT        GPIOB   /* SPI CS Pin */
 #define GPIO_11_PIN         6
-#define GPIO_11_CLKEN()     (RCC->AHBENR |= RCC_AHBENR_GPIOBEN)
+#define GPIO_11_CLKEN()     (periph_clk_en(AHB, RCC_AHBENR_GPIOBEN))
 #define GPIO_11_EXTI_CFG()  (SYSCFG->EXTICR[1] |= SYSCFG_EXTICR2_EXTI6_PB)
 #define GPIO_11_IRQ         EXTI9_5_IRQn
 /* GPIO channel 12 config */
 #define GPIO_12_PORT        GPIOC
 #define GPIO_12_PIN         5
-#define GPIO_12_CLKEN()     (RCC->AHBENR |= RCC_AHBENR_GPIOCEN)
+#define GPIO_12_CLKEN()     (periph_clk_en(AHB, RCC_AHBENR_GPIOCEN))
 #define GPIO_12_EXTI_CFG()  (SYSCFG->EXTICR[1] |= SYSCFG_EXTICR2_EXTI5_PC)
 #define GPIO_12_IRQ         EXTI9_5_IRQn
 /* GPIO channel 13 config */
 #define GPIO_13_PORT        GPIOA
 #define GPIO_13_PIN         0
-#define GPIO_13_CLKEN()     (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define GPIO_13_CLKEN()     (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define GPIO_13_EXTI_CFG()  (SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PA)
 #define GPIO_13_IRQ         EXTI0_IRQn
 /* GPIO channel 14 config */
 #define GPIO_14_PORT        GPIOA
 #define GPIO_14_PIN         1
-#define GPIO_14_CLKEN()     (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define GPIO_14_CLKEN()     (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define GPIO_14_EXTI_CFG()  (SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI1_PA)
 #define GPIO_14_IRQ         EXTI1_IRQn
 /* GPIO channel 15 config */
 #define GPIO_15_PORT        GPIOC
 #define GPIO_15_PIN         12
-#define GPIO_15_CLKEN()     (RCC->AHBENR |= RCC_AHBENR_GPIOCEN)
+#define GPIO_15_CLKEN()     (periph_clk_en(AHB, RCC_AHBENR_GPIOCEN))
 #define GPIO_15_EXTI_CFG()  (SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI12_PC)
 #define GPIO_15_IRQ         EXTI15_10_IRQn
 
@@ -284,12 +284,12 @@ static const timer_conf_t timer_config[] = {
 #define PWM_0_DEV           TIM2
 #define PWM_0_CHANNELS      4
 #define PWM_0_CLK           (32000000U)
-#define PWM_0_CLKEN()       (RCC->APB1ENR |= RCC_APB1ENR_TIM2EN)
-#define PWM_0_CLKDIS()      (RCC->APB1ENR &= ~(RCC_APB1ENR_TIM2EN))
+#define PWM_0_CLKEN()       (periph_clk_en(APB1, RCC_APB1ENR_TIM2EN))
+#define PWM_0_CLKDIS()      (periph_clk_dis(APB1, RCC_APB1ENR_TIM2EN))
 #define PWM_0_ISON()		(RCC->APB1ENR & RCC_APB1ENR_TIM2EN)
 /* PWM 0 pin configuration */
 #define PWM_0_PORT          GPIOA
-#define PWM_0_PORT_CLKEN()  (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define PWM_0_PORT_CLKEN()  (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define PWM_0_PIN_CH0       5
 #define PWM_0_PIN_CH1       1
 #define PWM_0_PIN_CH2       2
@@ -300,12 +300,12 @@ static const timer_conf_t timer_config[] = {
 #define PWM_1_DEV           TIM3
 #define PWM_1_CHANNELS      2
 #define PWM_1_CLK           (32000000U)
-#define PWM_1_CLKEN()       (RCC->APB1ENR |= RCC_APB1ENR_TIM3EN)
-#define PWM_1_CLKDIS()      (RCC->APB1ENR &= ~(RCC_APB1ENR_TIM3EN))
+#define PWM_1_CLKEN()       (periph_clk_en(APB1, RCC_APB1ENR_TIM3EN))
+#define PWM_1_CLKDIS()      (periph_clk_dis(APB1, RCC_APB1ENR_TIM3EN))
 #define PWM_1_ISON()		(RCC->APB1ENR & RCC_APB1ENR_TIM3EN)
 /* PWM 1 pin configuration */
 #define PWM_1_PORT          GPIOA
-#define PWM_1_PORT_CLKEN()  (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define PWM_1_PORT_CLKEN()  (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define PWM_1_PIN_CH0       6
 #define PWM_1_PIN_CH1       7
 #define PWM_1_PIN_CH2       0 /* do not use */
@@ -316,12 +316,12 @@ static const timer_conf_t timer_config[] = {
 #define PWM_2_DEV           TIM4
 #define PWM_2_CHANNELS      2
 #define PWM_2_CLK           (32000000U)
-#define PWM_2_CLKEN()       (RCC->APB1ENR |= RCC_APB1ENR_TIM4EN)
-#define PWM_2_CLKDIS()      (RCC->APB1ENR &= ~(RCC_APB1ENR_TIM4EN))
+#define PWM_2_CLKEN()       (periph_clk_en(APB1, RCC_APB1ENR_TIM4EN))
+#define PWM_2_CLKDIS()      (periph_clk_dis(APB1, RCC_APB1ENR_TIM4EN))
 #define PWM_2_ISON()		(RCC->APB1ENR & RCC_APB1ENR_TIM4EN)
 /* PWM 2 pin configuration */
 #define PWM_2_PORT          GPIOB
-#define PWM_2_PORT_CLKEN()  (RCC->AHBENR |= RCC_AHBENR_GPIOBEN)
+#define PWM_2_PORT_CLKEN()  (periph_clk_en(AHB, RCC_AHBENR_GPIOBEN))
 #define PWM_2_PIN_CH0       0 /* do not use */
 #define PWM_2_PIN_CH1       0 /* do not use */
 #define PWM_2_PIN_CH2       8
@@ -339,13 +339,13 @@ static const timer_conf_t timer_config[] = {
 
 /* SPI 0 device configuration */
 #define SPI_0_DEV           SPI1
-#define SPI_0_CLKEN()       (RCC->APB2ENR |= RCC_APB2ENR_SPI1EN)
-#define SPI_0_CLKDIS()      (RCC->APB2ENR  &= ~(RCC_APB2ENR_SPI1EN))
+#define SPI_0_CLKEN()       (periph_clk_en(APB2, RCC_APB2ENR_SPI1EN))
+#define SPI_0_CLKDIS()      (periph_clk_dis(APB2, RCC_APB2ENR_SPI1EN))
 #define SPI_0_ISON()		(RCC->APB2ENR & RCC_APB2ENR_SPI1EN)
 #define SPI_0_IRQ           SPI1_IRQn
 #define SPI_0_ISR           isr_spi1
 /* SPI 0 pin configuration */
-#define SPI_0_PORT_CLKEN()  (RCC->AHBENR |= RCC_AHBENR_GPIOAEN)
+#define SPI_0_PORT_CLKEN()  (periph_clk_en(AHB, RCC_AHBENR_GPIOAEN))
 #define SPI_0_PORT          GPIOA
 #define SPI_0_PIN_NSS       4
 #define SPI_0_PIN_SCK       5
@@ -356,14 +356,13 @@ static const timer_conf_t timer_config[] = {
 
 /* SPI 1 device configuration */
 #define SPI_1_DEV           SPI2
-#define SPI_1_CLKEN()       (RCC->APB1ENR |= RCC_APB1ENR_SPI2EN)
-#define SPI_1_ISON()        (RCC->APB1ENR & RCC_APB1ENR_SPI2EN)
-#define SPI_1_CLKDIS()      (RCC->APB1ENR &= ~(RCC_APB1ENR_SPI2EN))
+#define SPI_1_CLKEN()       (periph_clk_en(APB1, RCC_APB1ENR_SPI2EN))
+#define SPI_1_CLKDIS()      (periph_clk_dis(APB1, RCC_APB1ENR_SPI2EN))
 #define SPI_1_ISON()		(RCC->APB1ENR & RCC_APB1ENR_SPI2EN)
 #define SPI_1_IRQ           SPI2_IRQn
 #define SPI_1_ISR           isr_spi2
 /* SPI 1 pin configuration */
-#define SPI_1_PORT_CLKEN()  (RCC->AHBENR |= RCC_AHBENR_GPIOBEN)
+#define SPI_1_PORT_CLKEN()  (periph_clk_en(AHB, RCC_AHBENR_GPIOBEN))
 #define SPI_1_PORT          GPIOB
 #define SPI_1_PIN_NSS       12
 #define SPI_1_PIN_SCK       13

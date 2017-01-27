14c339d06c4a1e8ce45fc4349d53c258b1e254ee
SHEFF <sheff@sheff.com>
2017-01-27 16:52:28 +0300

    umdk-dali: Added checking type of address in command

--- unwired-modules/umdk-dali/include/umdk-dali.h
+++ unwired-modules/umdk-dali/include/umdk-dali.h
@@ -24,15 +24,19 @@
 
 #define UNWDS_DALI_MODULE_ID 16
 
+#define UMDK_DALI_1 UNWD_GPIO_4
+#define UMDK_DALI_2 UNWD_GPIO_5
+#define UMDK_DALI_3 UNWD_GPIO_6
+#define UMDK_DALI_4 UNWD_GPIO_7
+
 #define UMDK_DALI_CH_0 0
 #define UMDK_DALI_CH_1 1
 #define UMDK_DALI_CH_2 2
 #define UMDK_DALI_CH_3 3
 
-
-/**
- * @brief UMDK-DALI module commands list
- */
+#define UMDK_DALI_LENGTH_PACK 19
+#define UMDK_DALI_FREQUENCY_HZ 2400000
+#define UMDK_DALI_TIME_TX_PACK_USEC 416
 
 
 void umdk_dali_init(uint32_t *non_gpio_pin_map, uwnds_cb_t *event_callback);
--- unwired-modules/umdk-dali/umdk-dali.c
+++ unwired-modules/umdk-dali/umdk-dali.c
@@ -32,8 +32,17 @@ extern "C" {
 #include "unwds-common.h"
 #include "include/umdk-dali.h"
 
+#include "thread.h"
+#include "xtimer.h"
+
+static kernel_pid_t handler_pid;
 
 static uwnds_cb_t *callback;
+
+static xtimer_t pack_timer;
+static msg_t pack_msg;
+
+static uint32_t pack_dali;
 
 void umdk_dali_init(uint32_t *non_gpio_pin_map, uwnds_cb_t *event_callback)
 {
@@ -41,6 +50,18 @@ void umdk_dali_init(uint32_t *non_gpio_pin_map, uwnds_cb_t *event_callback)
 
     callback = event_callback;
 
+    gpio_init(UMDK_DALI_1, GPIO_IN_PU);
+
+    /* Create handler thread */
+     char *stack = (char *) allocate_stack();
+     if (!stack) {
+         puts("umdk-dali: unable to allocate memory. Is too many modules enabled?");
+         return;
+     }
+
+     handler_pid = thread_create(stack, UNWDS_STACK_SIZE_BYTES, THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST, handler, NULL, "Dali thread");
+
+     xtimer_set_msg(&pack_timer, UMDK_DALI_TIME_TX_PACK_USEC, &pack_msg, handler_pid);
 
 }
 
@@ -48,12 +69,36 @@ void umdk_dali_init(uint32_t *non_gpio_pin_map, uwnds_cb_t *event_callback)
 bool umdk_dali_cmd(module_data_t *cmd, module_data_t *reply)
 {
     /* Check minimum command length */
-    if (cmd->length < 6) {
+    if (cmd->length < 2) {
 	printf("[umdk-dali] Invalid command - wrong length of command\n");
         return false;
     }
 
-    return false;
+    /* Check on the address type */
+    /* if it isn't number of group or  isn't broadcast  */
+    if((cmd->data[0] >> 7) == 1){
+	if(( (cmd->data[0] >> 5) & 0x03) != 0) {
+	    /* if it isn't broadcast  */
+	    if((cmd->data[0] >> 1) != 0x7F) {
+		printf("[umdk-dali] Invalid broadcast command\n");
+		return false;
+	    }
+	    printf("[umdk-dali] Invalid type of address\n");
+	    return false;
+	}
+    }
+
+    /* Pack DALI is 19 bits */
+    pack_dali = ((0 << 18) + (0 << 17) + (cmd->data[1] << 9) + (cmd->data[0] << 1) + (1 << 0)) & 0x0007FFFF;
+
+    reply->length = 4;
+    reply->data[0] = UNWDS_DALI_MODULE_ID;
+    reply->data[1] = 'o';
+    reply->data[2] = 'k';
+    reply->data[3] = '\0';
+
+    return true; /* Allow reply */
+
 }
 
 

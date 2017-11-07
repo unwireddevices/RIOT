374c0491e0fd26fe42f7059cd648d4fd363d7460
Oleg Artamonov <oleg@unwds.com>
2017-11-07 17:19:23 +0300

    unwired-modules: do not init Connect Button if not configured

--- unwired-modules/umdk-opt3001/umdk-opt3001.c
+++ unwired-modules/umdk-opt3001/umdk-opt3001.c
@@ -216,8 +216,12 @@ void umdk_opt3001_init(uint32_t *non_gpio_pin_map, uwnds_cb_t *event_callback) {
 	}
     
     unwds_add_shell_command( _UMDK_NAME_, "type '" _UMDK_NAME_ "' for commands list", umdk_opt3001_shell_cmd);
-    
-    gpio_init_int(UNWD_CONNECT_BTN, GPIO_IN_PU, GPIO_FALLING, btn_connect, NULL);
+
+#ifdef UNWD_CONNECT_BTN
+    if (UNWD_USE_CONNECT_BTN) {
+        gpio_init_int(UNWD_CONNECT_BTN, GPIO_IN_PU, GPIO_FALLING, btn_connect, NULL);
+    }
+#endif
     
 	timer_pid = thread_create(stack, UNWDS_STACK_SIZE_BYTES, THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST, timer_thread, NULL, "opt3001 thread");
 

b4ed71dd7c18471e8afc684afcbf58968ca2c678
Oleg Artamonov <oleg@unwds.com>
2017-11-15 01:43:16 +0300

    boards/unwd-range: switch to TIM11 as TIM5 is not available on Cat. 1 & 2 devices

--- boards/unwd-range-l1/include/periph_conf.h
+++ boards/unwd-range-l1/include/periph_conf.h
@@ -61,18 +61,20 @@ extern "C" {
  * @brief Timer configuration
  * @{
  */
+#define TIMER_0_MAX_VALUE   (0x0000ffff)
+ 
 static const timer_conf_t timer_config[] = {
     {
-        .dev      = TIM5,
-        .max      = 0xffffffff,
-        .rcc_mask = RCC_APB1ENR_TIM5EN,
-        .bus      = APB1,
-        .irqn     = TIM5_IRQn
+        .dev      = TIM11,
+        .max      = TIMER_0_MAX_VALUE,
+        .rcc_mask = RCC_APB2ENR_TIM11EN,
+        .bus      = APB2,
+        .irqn     = TIM11_IRQn
     }
 };
 
-/* interrupt routines */
-#define TIMER_0_ISR         (isr_tim5)
+#define TIMER_0_ISR         isr_tim11
+
 /* number of defined timers */
 #define TIMER_NUMOF         (sizeof(timer_config) / sizeof(timer_config[0]))
 /** @} */

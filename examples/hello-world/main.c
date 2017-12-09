8a919761700515c0f8c5fdd5a8cfc38ef93f6e0a
Oleg Artamonov <oleg@unwds.com>
2017-12-09 13:27:34 +0300

    Thread and message example

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -21,12 +21,44 @@
 
 #include <stdio.h>
 
+#include "rtctimers-millis.h"
+#include "board.h"
+#include "periph/gpio.h"
+#include "thread.h"
+#include "lpm.h"
+
+static rtctimers_millis_t led_timer;
+static msg_t timer_msg = {};
+static kernel_pid_t led_pid;
+
+
+void *blink_a_led(void *arg) {
+    msg_t msg;
+    
+    while (1) {
+        msg_receive(&msg);
+        
+        gpio_toggle(LED_GREEN);
+        
+        rtctimers_millis_set_msg(&led_timer, 500, &timer_msg, led_pid);
+    }
+}
+
 int main(void)
 {
+    lpm_prevent_sleep = 1;
+    
+    rtctimers_millis_init();
+    
     puts("Hello World!");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
+    char stack[2048];
+    led_pid = thread_create(stack, 2048, THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST, blink_a_led, NULL, "led thread");
+    
+    rtctimers_millis_set_msg(&led_timer, 500, &timer_msg, led_pid);
 
     return 0;
 }

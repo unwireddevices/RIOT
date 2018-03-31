f077a27ac44989983fb0f9fce7fb2f9ea0ea56da
Oleg Artamonov <oleg@unwds.com>
2018-03-31 17:30:57 +0300

    Lesson 3

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -18,36 +18,79 @@
  *
  * @}
  */
-
 #include <stdio.h>
 
 #include "periph/gpio.h"
+#include "periph/pm.h"
 #include "rtctimers-millis.h"
+#include "shell_commands.h"
+#include "shell.h"
+#include "thread.h"
+
+
+
+static kernel_pid_t process_pid;
+static msg_t process_msg;
+
+#define MY_PROCESS_STACK_SIZE (1024)
+
+#define ENABLE_DEBUG    (1)
+#include "debug.h"
 
 uint32_t milliseconds_last_press = 0;
 
+static void *process_thread(void *arg) {
+    (void)arg;
+    msg_t message;
+    gpio_set(GPIO_PIN(PORT_B, 0));
+    while (1) {
+        msg_receive(&message);
+        gpio_toggle(GPIO_PIN(PORT_B, 0));
+        DEBUG("LED set to %d\n", gpio_read(GPIO_PIN(PORT_B, 0)));
+    }
+    return NULL;
+}
+
 static void btn_led_toggle(void* arg) {
     (void)arg;
-    
-    if ((rtctimers_millis_now() - milliseconds_last_press) > 100) {        
-        gpio_toggle(GPIO_PIN(PORT_B, 0));
+    if ((rtctimers_millis_now() - milliseconds_last_press) > 200) {        
+        msg_send(&process_msg, process_pid);
         milliseconds_last_press = rtctimers_millis_now();
     }
 }
 
+static const shell_command_t shell_commands[] = {
+    { NULL, NULL, NULL }
+};
+
 int main(void)
 {
-    rtctimers_millis_init();
+    pm_init();
+    pm_prevent_sleep = 1;
     
+    rtctimers_millis_init();
+
     puts("Hello World!");
 
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
-    
+
     gpio_init(GPIO_PIN(PORT_B, 0), GPIO_OUT);
-    gpio_set(GPIO_PIN(PORT_B, 0));
     
     gpio_init_int(GPIO_PIN(PORT_B, 1), GPIO_IN_PU, GPIO_FALLING, btn_led_toggle, NULL);
+    
+    char str[10];
+    printf("%s\n", str);
 
+    char stack[MY_PROCESS_STACK_SIZE];
+    process_pid = thread_create(stack, MY_PROCESS_STACK_SIZE,
+                                THREAD_PRIORITY_MAIN - 1, THREAD_CREATE_STACKTEST,
+                                process_thread, NULL, "our process");
+    
+    
+    /* shell initialization */
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    
     return 0;
 }

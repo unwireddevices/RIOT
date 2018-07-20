86a62acca09251f2c3c62d0de47d7b104b54e0d5
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-07-20 13:50:54 +0200

    drivers/sx127x: remove xtimer timeout from device timeout

--- drivers/sx127x/sx127x_getset.c
+++ drivers/sx127x/sx127x_getset.c
@@ -905,8 +905,6 @@ void sx127x_set_symbol_timeout(sx127x_t *dev, uint16_t timeout)
 {
     DEBUG("[sx127x] Set symbol timeout: %d\n", timeout);
 
-    dev->settings.lora.rx_timeout = timeout;
-
     uint8_t config2_reg = sx127x_reg_read(dev, SX127X_REG_LR_MODEMCONFIG2);
     config2_reg &= SX127X_RF_LORA_MODEMCONFIG2_SYMBTIMEOUTMSB_MASK;
     config2_reg |= (timeout >> 8) & ~SX127X_RF_LORA_MODEMCONFIG2_SYMBTIMEOUTMSB_MASK;

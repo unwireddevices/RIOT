a348781a1f631afd0e056a9ce28695e68e840787
Oleg Artamonov <oleg@unwds.com>
2018-08-27 22:47:58 +0300

    boards: cleanup boards configuration

--- boards/unwd-range-l0/include/periph_conf.h
+++ boards/unwd-range-l0/include/periph_conf.h
@@ -91,18 +91,6 @@ static const timer_conf_t timer_config[] = {
  * @brief UART configuration
  */
 static const uart_conf_t uart_config[] = {
-    // {
-    //     .dev      = USART1,
-    //     .rcc_mask = RCC_APB2ENR_USART1EN,
-    //     .rx_pin   = GPIO_PIN(PORT_A, 10),
-    //     .tx_pin   = GPIO_PIN(PORT_A, 9),
-    //     .rx_mode  = GPIO_IN_PU,
-    //     .tx_mode  = GPIO_OUT,
-    //     .rx_af    = GPIO_AF4,
-    //     .tx_af    = GPIO_AF4,
-    //     .bus      = APB2,
-    //     .irqn     = USART1_IRQn
-    // },
     {
         .dev      = USART2,
         .rcc_mask = RCC_APB1ENR_USART2EN,

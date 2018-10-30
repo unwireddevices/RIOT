f4b54de8626687086fe8239e4157b0c095ba1c90
SHEFF <misha-37@mail.ru>
2018-10-30 17:00:28 +0300

    st95 driver

--- drivers/include/st95.h
+++ drivers/include/st95.h
@@ -26,6 +26,9 @@
 #include "periph/gpio.h"
 #include "periph/spi.h"
 
+#define ST95_IDLE_STATE 0
+#define ST95_READY_STATE 1
+
 /**
  * @brief ST95 return codes
 */
@@ -46,6 +49,18 @@ typedef struct {
 } st95_params_t;
 
 /**
+ * @brief   ADCxx1C alert callback
+ */
+typedef void (*st95_cb_t)(void *);
+
+/**
+ * @brief   ST95 device descriptor
+ */
+typedef struct {
+    st95_params_t params;
+    st95_cb_t cb;
+} st95_t;
+/**
  * @brief ST95 bus driver initialization routine
  *
  * @param[in] Device (SPI) to be used for ST95 communication
@@ -53,12 +68,17 @@ typedef struct {
  * @return 1 if initialization succeeded
  * @return <0 in case of an error
  */
-int st95_init(st95_params_t * device);
+int st95_init(st95_t *dev, st95_params_t * params);
 
-int st95_echo(void);
-int st95_idn(uint8_t * idn, uint8_t * length);
-uint8_t st95_idle(void);
-void st95_send_receive(uint8_t *data, uint8_t size, uint8_t topaz, uint8_t split_frame, uint8_t crc, uint8_t sign_bits);
-uint8_t st95_receive(uint8_t * rxbuff);
-int st95_get_uid(uint8_t * length_uid, uint8_t * uid, uint8_t * sak);
+void st95_sleep(st95_t * dev);
+
+int st95_is_wake_up(const st95_t * dev);
+
+int st95_idn(const st95_t * dev, uint8_t * idn, uint8_t * length);
+
+int st95_get_uid(const st95_t * dev, uint8_t * length_uid, uint8_t * uid, uint8_t * sak);
+
+int st95_send_receive(const st95_t * dev, uint8_t *data, uint8_t size, uint8_t topaz, uint8_t split_frame, uint8_t crc, uint8_t sign_bits);
+// uint8_t st95_receive(uint8_t * rxbuff);
+
 #endif /* ST95_H_ */

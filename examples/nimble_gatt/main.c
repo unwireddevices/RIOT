22bdb6cffac3445abb27a07347c557eeb8f38f2f
Hauke Petersen <hauke.petersen@fu-berlin.de>
2019-03-07 16:22:56 +0100

    examples/nimble_gatt: unroll update_ad()
    This static function was a leftover from very early stages of this
    example and at the current state there is not sense in keeping it.
    Having these few lines directly where the advertising is triggered
    makes the code more clean and readible.

--- examples/nimble_gatt/main.c
+++ examples/nimble_gatt/main.c
@@ -132,15 +132,6 @@ static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
     },
 };
 
-static void update_ad(void)
-{
-    uint8_t buf[BLE_HS_ADV_MAX_SZ];
-    bluetil_ad_t ad;
-    bluetil_ad_init_with_flags(&ad, buf, sizeof(buf), BLUETIL_AD_FLAGS_DEFAULT);
-    bluetil_ad_add_name(&ad, device_name);
-    ble_gap_adv_set_data(ad.buf, ad.pos);
-}
-
 static int gap_event_cb(struct ble_gap_event *event, void *arg)
 {
     (void)arg;
@@ -320,27 +311,8 @@ int main(void)
 
     int rc = 0;
 
+    /* verify and add our custom services */
     rc = ble_gatts_count_cfg(gatt_svr_svcs);
-    assert(rc == 0);
-
-    rc = ble_gatts_add_svcs(gatt_svr_svcs);
-    assert(rc == 0);
-
-    /* set the device name */
-    ble_svc_gap_device_name_set(device_name);
-
-    /* initialize the GAP and GATT services */
-    ble_svc_gap_init();
-    ble_svc_gatt_init();
-    /* XXX: seems to be needed to apply the added services */
-    ble_gatts_start();
-
-    /* make sure synchronization of host and controller is done, this should
-     * always be the case */
-    while (!ble_hs_synced()) {}
-
-    /* configure device address */
-    rc = ble_hs_util_ensure_addr(0);
     assert(rc == 0);
     rc = ble_gatts_add_svcs(gatt_svr_svcs);
     assert(rc == 0);

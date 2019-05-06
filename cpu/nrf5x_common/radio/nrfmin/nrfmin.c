605c2c495f6908a9e2032bf8c1dde297dbf929c2
Semjon Kerner <semjon.kerner@fu-berlin.de>
2019-05-06 15:33:30 +0200

    cpu/nrf5x/radio/nrfmin: explicit test for power mode

--- cpu/nrf5x_common/radio/nrfmin/nrfmin.c
+++ cpu/nrf5x_common/radio/nrfmin/nrfmin.c
@@ -328,7 +328,10 @@ static int nrfmin_send(netdev_t *dev, const iolist_t *iolist)
 {
     (void)dev;
 
-    assert((iolist) && (state != STATE_OFF));
+    assert(iolist);
+    if (state == STATE_OFF) {
+        return -ENETDOWN;
+    }
 
     /* wait for any ongoing transmission to finish and go into idle state */
     while (state == STATE_TX) {}
@@ -371,7 +374,9 @@ static int nrfmin_recv(netdev_t *dev, void *buf, size_t len, void *info)
     (void)dev;
     (void)info;
 
-    assert(state != STATE_OFF);
+    if (state == STATE_OFF) {
+        return -ENETDOWN;
+    }
 
     unsigned pktlen = rx_buf.pkt.hdr.len;
 

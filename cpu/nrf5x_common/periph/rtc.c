40e00fd5795ae7e228849426e720534f29f34d00
Oleg Artamonov <oleg@unwds.com>
2019-05-15 17:58:06 +0300

    cpu/nrf5x_common: minimum usable RTC implemenation

--- cpu/nrf5x_common/periph/rtc.c
+++ cpu/nrf5x_common/periph/rtc.c
@@ -10,13 +10,14 @@
  * @ingroup     cpu_nrf5x_common
  * @{
  * @file
- * @brief       Low-level RTC driver implementation based on RTT hardware
+ * @brief       Low-level RTC driver implementation based on Nordic RTT hardware
  *
  * @author      Oleg Artamonov <oleg@unwds.com>
  * @}
  */
 
 #include <time.h>
+#include <string.h>
 #include "cpu.h"
 #include "mutex.h"
 #include "periph/rtt.h"
@@ -25,7 +26,21 @@
 #define ENABLE_DEBUG    (0)
 #include "debug.h"
 
+/* 
+ * With 1024 Hz RTT frequency one tick is 976.5625 us
+ */
+
+#define RTT_TICK_US (1000000UL / RTT_FREQUENCY)
+#define RTC_WEEK_MILLISECONDS    604800000
+
 static mutex_t rtc_mutex;
+static uint64_t time_epoch_us = 0;
+
+static struct {
+    uint64_t        time_next_alarm;
+    rtc_alarm_cb_t  cb;
+    void*           arg;
+} rtc_next_timer;
 
 static struct {
     rtc_alarm_cb_t cb_a;        /**< callback called from RTC interrupt */
@@ -37,7 +52,7 @@ static struct {
     void *arg_wkup;             /**< argument passed to wakeup callback */
 } isr_ctx;
 
-void rtc_relese(void) {
+void rtc_release(void) {
     mutex_unlock(&rtc_mutex);
 }
 
@@ -45,8 +60,19 @@ void rtc_acquire(void) {
     mutex_lock(&rtc_mutex);
 }
 
-void rtc_callback(void* arg)
+void rtc_ovf_callback(void* arg)
 {
+    (void) arg;
+
+    /* timer overflows every 2e24 * 976.5625 us = 16 384 000 000 us = ~4.55 hours */
+    time_epoch_us += 16384000000;
+    
+    if ((rtc_next_timer.time_next_alarm - time_epoch_us) <= (RTT_MAX_VALUE * RTT_TICK_US)) {
+        
+        uint32_t target = (rtc_next_timer.time_next_alarm - time_epoch_us) / RTT_TICK_US;
+        
+        rtt_set_alarm(target, rtc_next_timer.cb, rtc_next_timer.arg);
+    }
 }
 
 void rtc_init(void)
@@ -57,12 +83,18 @@ void rtc_init(void)
     /* enable low frequency clock */
     rtt_init();
     
+    /* overflow callback */
+    rtt_set_overflow_cb(&rtc_ovf_callback, NULL);
+    
     rtc_release();
 }
 
 int rtc_set_time(struct tm *time)
 {
+    /* not implemented yet */
     rtc_acquire();
+
+    (void) time;
     
     rtc_release();
 
@@ -73,6 +105,9 @@ int rtc_get_time(struct tm *time)
 {
     rtc_acquire();
     
+    time_t epoch = (time_epoch_us + ((uint64_t)rtt_get_counter() * RTT_TICK_US)) / 1000000;
+    memcpy(&time, gmtime(&epoch), sizeof(struct tm));
+    
     rtc_release();
 
     return 0;
@@ -80,7 +115,12 @@ int rtc_get_time(struct tm *time)
 
 int rtc_set_alarm(struct tm *time, rtc_alarm_cb_t cb, void *arg)
 {
+    /* not implemented yet */
     rtc_acquire();
+    
+    (void) time;
+    (void) cb;
+    (void) arg;
     
     rtc_release();
 
@@ -89,7 +129,10 @@ int rtc_set_alarm(struct tm *time, rtc_alarm_cb_t cb, void *arg)
 
 int rtc_get_alarm(struct tm *time)
 {
+    /* not implemented yet */
     rtc_acquire();
+    
+    (void) time;
     
     rtc_release();
 
@@ -98,6 +141,7 @@ int rtc_get_alarm(struct tm *time)
 
 void rtc_clear_alarm(void)
 {
+    /* not implemented yet */
     rtc_acquire();
 
     isr_ctx.cb_a = NULL;
@@ -109,9 +153,17 @@ void rtc_clear_alarm(void)
 int rtc_millis_set_alarm(uint32_t milliseconds, rtc_alarm_cb_t cb, void *arg)
 {   
     rtc_acquire();
-
-    isr_ctx.cb_b = cb;
-    isr_ctx.arg_b = arg;
+    
+    uint32_t now = rtt_get_counter();
+    uint32_t target = now + ((uint64_t)milliseconds * 1000)/RTT_TICK_US;
+    
+    if (target <= RTT_MAX_VALUE) {
+        rtt_set_alarm(target - now, cb, arg);
+    } else {
+        rtc_next_timer.time_next_alarm = time_epoch_us + now*RTT_TICK_US + milliseconds*1000;
+        rtc_next_timer.arg = arg;
+        rtc_next_timer.cb = cb;
+    }
 
     rtc_release();
     return 0;
@@ -131,13 +183,21 @@ int rtc_millis_get_time(uint32_t *millis)
 {
     rtc_acquire();
     
+    /* 1 week overflow */
+    *millis = ((time_epoch_us + rtt_get_counter() * RTT_TICK_US)/1000) % RTC_WEEK_MILLISECONDS;
+    
     rtc_release();
     return 0;
 }
 
 int rtc_set_wakeup(uint32_t period_us, rtc_wkup_cb_t cb, void *arg)
 {
+    /* not implemented yet */
     rtc_acquire();
+    
+    (void) period_us;
+    (void) cb;
+    (void) arg;
 
     isr_ctx.cb_wkup = cb;
     isr_ctx.arg_wkup = arg;
@@ -148,6 +208,7 @@ int rtc_set_wakeup(uint32_t period_us, rtc_wkup_cb_t cb, void *arg)
 }
 
 void rtc_enable_wakeup(void) {
+    /* not implemented yet */
     rtc_acquire();
 
     rtc_release();
@@ -155,6 +216,7 @@ void rtc_enable_wakeup(void) {
 
 void rtc_disable_wakeup(void)
 {
+    /* not implemented yet */
     rtc_acquire();
 
     rtc_release();

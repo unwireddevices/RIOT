86af4e161de699398d508794a0c7acc277907b71
Oleg Artamonov <oleg@unwds.com>
2019-06-06 08:15:43 +0300

    drivers/fdc1004: gain and capdac settings added

--- drivers/fdc1004/fdc1004.c
+++ drivers/fdc1004/fdc1004.c
@@ -44,6 +44,12 @@ int fdc1004_init(fdc1004_t *dev)
 
     i2c_init(dev->i2c);
     
+    for (int i = 0; i < 4; i++) {
+        if ((dev->offset[i] > 96) || (dev->gain[i] > 3163)) {
+            return -2;
+        }
+    }
+    
     if (i2c_read_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_VENDOR_ID, (char *)&chipid, 2, 0) != 0) {
         puts("[fdc1004] Error: sensor not found");
         i2c_release(dev->i2c);
@@ -87,25 +93,41 @@ uint32_t fdc1004_get_raw_data(fdc1004_t *dev, uint8_t channel)
     assert(dev != NULL);
     
     channel -= 1;
-
+    
     /* Acquire the I2C bus */
     i2c_acquire(dev->i2c);
     
     uint16_t reg;
+    
+    /* convert pF to CAPDAC value */
+    uint32_t capdac = ((uint32_t)dev->offset[channel]*1000)/3125;
     
     DEBUG("[fdc1004] setting channel %d\n", channel);
     
     /* set positive channel */
     reg = (channel << 13);
     
-    /* disable negative channel */
-    reg |= (0b111 << 10);
+    if (capdac) {
+        /* set CAPDAC value and enable CAPDAC */
+        reg |= (capdac << 5) | (4 << 10);
+    } else {
+        /* disable negative channel */
+        reg |= (0b111 << 10);
+    }
+    
     reg = byteorder_swaps(reg);
     i2c_write_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_CONF_MEAS1 + channel, (uint8_t *)&reg, 2, 0);
     
+    DEBUG("[fdc1004] setting gain\n");   
+    uint8_t gain_int = (dev->gain[channel] / 1000);
+    uint8_t gain_frc = (dev->gain[channel] % 1000);
+    
+    reg = (gain_int << 14) | (gain_frc << 4);
+    reg = byteorder_swaps(reg);
+    i2c_write_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_GAIN_CAL_CIN1 + channel, (uint8_t *)&reg, 2, 0);
+    
     DEBUG("[fdc1004] setting sample rate\n");
-    /* set sample rate to 100 S/s */
-    reg = (0b01 << 10);
+    reg = (dev->rate << 10);
     /* repeated measurements */
     reg |= (1 << 8);
     
@@ -116,46 +138,36 @@ uint32_t fdc1004_get_raw_data(fdc1004_t *dev, uint8_t channel)
     reg = byteorder_swaps(reg);
     i2c_write_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_FDC_CONF, (uint8_t *)&reg, 2, 0);
     
-    int counter = 0;
-    uint32_t capacitance = 0;
-    
-    /* wait for result */
+    DEBUG("[fdc1004] waiting for result\n");
     do {
-        DEBUG("[fdc1004] waiting for result\n");
-        do {
-            rtctimers_millis_sleep(10);
-            i2c_read_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_FDC_CONF, (uint8_t *)&reg, 2, 0);
-            reg = byteorder_swaps(reg);
-        } while (!(reg & (1 << (3 - channel))));
-        
-        DEBUG("[fdc1004] reading result\n");
-        
-        /* Read capacitance data */
-        uint32_t cap = 0;
-        
-        i2c_read_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_MEAS1_LSB + (channel * 2), (uint8_t *)&reg, 2, 0);
-        cap = byteorder_swaps(reg);
-        DEBUG("[fdc1004] LSB: 0x%04x\n", byteorder_swaps(reg));
-        i2c_read_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_MEAS1_MSB + (channel * 2), (uint8_t *)&reg, 2, 0);
-        cap |= byteorder_swaps(reg) << 16;
-        DEBUG("[fdc1004] MSB: 0x%04x\n", byteorder_swaps(reg));
-        cap >>= 8;
-        
-        capacitance += cap;
-    } while (counter++ < 8);
+        rtctimers_millis_sleep(5);
+        i2c_read_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_FDC_CONF, (uint8_t *)&reg, 2, 0);
+        reg = byteorder_swaps(reg);
+    } while (!(reg & (1 << (3 - channel))));
+    
+    DEBUG("[fdc1004] reading result\n");
+    
+    /* Read capacitance data */
+    uint32_t cap = 0;
+    
+    i2c_read_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_MEAS1_LSB + (channel * 2), (uint8_t *)&reg, 2, 0);
+    cap = byteorder_swaps(reg);
+    DEBUG("[fdc1004] LSB: 0x%04x\n", byteorder_swaps(reg));
+    i2c_read_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_MEAS1_MSB + (channel * 2), (uint8_t *)&reg, 2, 0);
+    cap |= byteorder_swaps(reg) << 16;
+    DEBUG("[fdc1004] MSB: 0x%04x\n", byteorder_swaps(reg));
+    cap >>= 8;
     
     /* reset the device */
     reg = 1 << 15;
     i2c_write_regs(dev->i2c, FDC1004_ADDRESS, FDC1004_REG_FDC_CONF, (uint8_t *)&reg, 2, 0);
-    
-    capacitance >>= 3;
-    
-    DEBUG("[fdc1004] capacitance: %lu\n", capacitance);
+
+    DEBUG("[fdc1004] capacitance: %lu\n", cap);
 
     /* Release the I2C bus */
     i2c_release(dev->i2c);
 
-    return capacitance;
+    return cap;
 }
 
 

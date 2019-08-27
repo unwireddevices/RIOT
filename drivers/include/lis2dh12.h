e1a271c0b56dd9d1aa0dfb261f720b5c350a8f6e
Alexander Ugorelov <alex_u@unwds.com>
2019-08-27 14:29:37 +0300

    drivers/lis2dh12: Implement a new driver version

--- drivers/include/lis2dh12.h
+++ drivers/include/lis2dh12.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Freie Universität Berlin
+ * Copyright (C) 2019 Unwired Devices [info@unwds.com]
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -9,147 +9,182 @@
 /**
  * @defgroup    drivers_lis2dh12 LIS2DH12 Accelerometer
  * @ingroup     drivers_sensors
- * @ingroup     drivers_saul
  * @brief       Driver for the STM LIS2DH12 accelerometer
  *
- * This device driver provides a minimal interface to LIS2DH12 devices. As of
- * now, it only provides very basic access to the device. The driver configures
- * the device to continuously read the acceleration data with statically
- * defined scale and rate, and with a fixed 10-bit resolution. The LIS2DH12's
+ * This device driver provides a minimal interface to LIS2HH12 devices. As of
+ * now, it only provides very basic access to the device. The LIS2HH12's
  * FIFO is bypassed, so the driver might not be sufficient for use cases where
  * the complete history of readings is of interest.
  *
  * Also, the current version of the driver supports only interfacing the sensor
- * via SPI. The driver is however written in a way, that adding I2C interface
- * support is quite simple, as all bus related functions (acquire, release,
- * read, write) are cleanly separated in the code.
- *
- * This driver provides @ref drivers_saul capabilities.
+ * via I2C.
  *
  * @{
  * @file
- * @brief       Interface definition for the STM LIS2DH12 accelerometer
+ * @brief       Interface definition for the STMicro LIS2DH12 accelerometer
  *
- * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Alexander Ugorelov <info@unwds.com>
  */
 
 #ifndef LIS2DH12_H
 #define LIS2DH12_H
 
-#include "saul.h"
-#include "periph/spi.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "periph/i2c.h"
 #include "periph/gpio.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* I2C support is not implemented (yet), so throw an error when selected */
-#ifndef MODULE_LIS2DH12_SPI
-#error "LIS2DH12 error: I2C mode is not supported, yet. Use module li2dh12_spi"
+/* SPI support is not implemented (yet), so throw an error when selected */
+#ifdef MODULE_LIS2DH12_SPI
+#error "LIS2HH12 error: SPI mode is not supported yet."
 #endif
+
+/**
+ * @name    Set default configuration parameters for LIS2DH12 devices
+ * @{
+ */
+#define LIS2DH12_SAD0L               (0x00)
+#define LIS2DH12_SAD0H               (0x01)
+#define LIS2DH12_I2C_SADROOT         (0x03)
+
+/* I2C address if acc SA0 pin to GND */
+#define LIS2DH12_I2C_SAD_L           ((LIS3DH_I2C_SADROOT << 3) | \
+                                      LIS3DH_SAD0L)
+
+/* I2C address if acc SA0 pin to Vdd */
+#define LIS2DH12_I2C_SAD_H           ((LIS3DH_I2C_SADROOT << 3) | \
+                                      LIS3DH_SAD0H)
 
 /**
  * @brief   Available scale values
  */
 typedef enum {
-    LIS2DH12_SCALE_2G  = 0x00,      /**< +- 2g */
-    LIS2DH12_SCALE_4G  = 0x10,      /**< +- 4g */
-    LIS2DH12_SCALE_8G  = 0x20,      /**< +- 8g */
-    LIS2DH12_SCALE_16G = 0x30,      /**< +- 16g */
+    LIS2DH12_SCALE_2G  = 0x00,                          /**< +- 2g */
+    LIS2DH12_SCALE_4G  = 0x01,                          /**< +- 4g */
+    LIS2DH12_SCALE_8G  = 0x02,                          /**< +- 8g */
+    LIS2DH12_SCALE_16G = 0x03,                          /**< +- 16g */
 } lis2dh12_scale_t;
 
 /**
  * @brief   Available sampling rates
- *
- * @note    The device does also support some additional rates for specific low-
- *          power modes, but those are as of now not supported by this driver
  */
 typedef enum {
-    LIS2DH12_RATE_POWER_DOWN = 0x00,
-    LIS2DH12_RATE_1HZ        = 0x17,     /**< sample with 1Hz */
-    LIS2DH12_RATE_10HZ       = 0x27,     /**< sample with 10Hz */
-    LIS2DH12_RATE_25HZ       = 0x37,     /**< sample with 25Hz */
-    LIS2DH12_RATE_50HZ       = 0x47,     /**< sample with 50Hz */
-    LIS2DH12_RATE_100HZ      = 0x57,     /**< sample with 100Hz */
-    LIS2DH12_RATE_200HZ      = 0x67,     /**< sample with 200Hz */
-    LIS2DH12_RATE_400HZ      = 0x77,     /**< sample with 400Hz */
+    LIS2DH12_RATE_POWER_DOWN = 0x00,                    /**< b0000: Power-down mode                       */
+    LIS2DH12_RATE_1HZ        = 0x01,                    /**< b0001: HR / Normal / Low-power mode (1 Hz)   */
+    LIS2DH12_RATE_10HZ       = 0x02,                    /**< b0010: HR / Normal / Low-power mode (10 Hz)  */
+    LIS2DH12_RATE_25HZ       = 0x03,                    /**< b0011: HR / Normal / Low-power mode (25 Hz)  */
+    LIS2DH12_RATE_50HZ       = 0x04,                    /**< b0100: HR / Normal / Low-power mode (50 Hz)  */
+    LIS2DH12_RATE_100HZ      = 0x05,                    /**< b0101: HR / Normal / Low-power mode (100 Hz) */
+    LIS2DH12_RATE_200HZ      = 0x06,                    /**< b0110: HR / Normal / Low-power mode (200 Hz) */
+    LIS2DH12_RATE_400HZ      = 0x07,                    /**< b0111: HR / Normal / Low-power mode (400 Hz) */
+    LIS2DH12_RATE_1620Hz     = 0x08,                    /**< b1000: Low-power mode (1.620 kHz)            */
+    LIS2DH12_RATE_5376Hz     = 0x09,                    /**< b1001: HR/ Normal (1.344 kHz) Low-power mode (5.376 kHz) */
 } lis2dh12_rate_t;
+
+/**
+ * @brief   Resolution (operating) modes
+ */
+typedef enum {
+    LIS2DH12_HR_12BIT   = 0x00,                         /**< High-resolution mode */
+    LIS2DH12_NM_10BIT   = 0x01,                         /**< Normal mode */
+    LIS2DH12_LP_8BIT    = 0x02,                         /**< Low-power mode */
+} lis2dh12_resolution_t;
 
 /**
  * @brief   LIS2DH12 configuration parameters
  */
 typedef struct {
-    spi_t spi;                      /**< SPI bus the device is connected to */
-    gpio_t cs;                      /**< connected chip select pin */
-    lis2dh12_scale_t scale;         /**< sampling sensitivity used */
-    lis2dh12_rate_t rate;           /**< sampling rate used */
+    i2c_t                 i2c_dev;                      /**< I2C device, clock stretching required (default I2C_DEV(0)) */
+    uint8_t               i2c_addr;                     /**< I2C address (default LIS2DH12_I2C_SAD_L) */
+    lis2dh12_scale_t      scale;                        /**< Sensor scale: 2, 4, 8, or 16 (G) */
+    lis2dh12_rate_t       rate;                         /**< Sensor ODR setting: LIS3DH_ODR_xxxHz */
+    lis2dh12_resolution_t res;                          /**< Resolution (operation mode) */
 } lis2dh12_params_t;
 
 /**
  * @brief   LIS2DH12 device descriptor
  */
 typedef struct {
-    const lis2dh12_params_t *p;     /**< device configuration */
-    uint16_t comp;                  /**< scale compensation factor */
+    lis2dh12_params_t params;                           /**<Device initialization parameters */
+    bool              use_temp;                         /**< Use temperature measurement? */
+    uint16_t          comp;                             /**< scale compensation factor */
 } lis2dh12_t;
 
 /**
- * @brief   Status and error return codes
+ * @brief   Driver error codes (returned as negative values)
  */
-enum {
-    LIS2DH12_OK    =  0,            /**< everything was fine */
-    LIS2DH12_NOBUS = -1,            /**< bus interface error */
-    LIS2DH12_NODEV = -2,            /**< unable to talk to device */
-};
+typedef enum {
+    LIS2DH12_OK,                                        /**< no error */
+    LIS2DH12_ERROR_I2C,                                 /**< I2C communication failure */
+    LIS2DH12_ERROR_NO_DEV,                              /**< device not available */
+    LIS2DH12_ERROR_NO_NEW_DATA,                         /**< no new data (last valid data returned) */
+    LIS2DH12_ERROR_NOT_SUPPORTED,                       /**< function is not supported */
+} lis2dh12_error_codes_t;
 
 /**
- * @brief   Export the SAUL interface for this driver
+ * @brief   Result vector for accelerometer measurement
  */
-extern const saul_driver_t lis2dh12_saul_driver;
+typedef struct {
+    int16_t axis_x;                                     /**< Acceleration in the X direction in milli-G */
+    int16_t axis_y;                                     /**< Acceleration in the Y direction in milli-G */
+    int16_t axis_z;                                     /**< Acceleration in the Z direction in milli-G */
+} lis2dh12_acc_t;
 
 /**
  * @brief   Initialize the given LIS2DH12 sensor device
  *
- * @param[out] dev      device descriptor
- * @param[in]  params   static device configuration
+ * @param[in/out] dev       Device descriptor of sensor to initialize
+ * @param[in]     params    Configuration parameters
  *
- * @return  LIS2DH12_OK on success
- * @return  LIS2DH12_NOBUS on bus errors
- * @return  LIS2DH12_NODEV if no LIS2DH12 device was found on the bus
+ * @return                  Error status
  */
 int lis2dh12_init(lis2dh12_t *dev, const lis2dh12_params_t *params);
 
 /**
- * @brief   Read acceleration data from the given device
+ * @brief   Read 3D acceleration data from the accelerometer
  *
- * @param[in]  dev      device descriptor
- * @param[out] data     acceleration data in mili-g, **MUST** hold 3 values
+ * @param[in]  dev            Device descriptor of sensor
+ * @param[out] acceleration   Accelerometer data output
  *
- * @return  LIS2DH12_OK on success
- * @return  LIS2DH12_NOBUS on bus error
+ * @return                    Error status
  */
-int lis2dh12_read(const lis2dh12_t *dev, int16_t *data);
+int lis2dh12_read_xyz(lis2dh12_t *dev, lis2dh12_acc_t *acceleration);
+
+/**
+ * @brief   Read temperature from the accelerometer
+ * 
+ * @param dev                Device descriptor of sensor
+ * @param temperature_degC   Temperature output
+ * 
+ * @return                   Error status
+ */
+int lis2dh12_read_temp(lis2dh12_t *dev, int16_t *temperature_degC);
 
 /**
  * @brief   Power on the given device
- *
- * @param[in] dev       device descriptor
- *
- * @return  LIS2DH12_OK on success
- * @return  LIS2DH12_NOBUS on bus error
+ * 
+ * @param[in/out] dev   Device descriptor of sensor
+ * @param[in]     scale Output data scale
+ * @param[in]     rate  Output data rate
+ * @param[in]     res   Output data resolution
+ * 
+ * @return           Error status
  */
-int lis2dh12_poweron(const lis2dh12_t *dev);
+int lis2dh12_power_on(lis2dh12_t *dev, lis2dh12_scale_t scale, lis2dh12_rate_t rate, lis2dh12_resolution_t res);
 
 /**
  * @brief   Power off the given device
  *
- * @param[in] dev       device descriptor
+ * @param[in/out] dev   Device descriptor of sensor
  *
- * @return  LIS2DH12_OK on success
- * @return  LIS2DH12_NOBUS on bus error
+ * @return  Error status
  */
-int lis2dh12_poweroff(const lis2dh12_t *dev);
+int lis2dh12_power_off(lis2dh12_t *dev);
 
 #ifdef __cplusplus
 }

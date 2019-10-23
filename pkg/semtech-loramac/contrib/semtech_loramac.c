fe2fd1ed7b0078087eb3623ea44698224aa274b5
Oleg Artamonov <oleg@unwds.com>
2019-10-23 10:52:11 +0300

    pkg/semtech-loramac: do not use CadDone if not needed

--- pkg/semtech-loramac/contrib/semtech_loramac.c
+++ pkg/semtech-loramac/contrib/semtech_loramac.c
@@ -195,7 +195,10 @@ void _init_loramac(semtech_loramac_t *mac,
     LoRaMacInitialization(&semtech_loramac_radio_events, primitives, callbacks,
                           LORAMAC_ACTIVE_REGION);
                           
+#if defined(LORAMAC_CHANNEL_FREE_DETECT_PREAMBLE)
     semtech_loramac_radio_events.CadDone = sx127x_lora_cad_done;
+#endif
+
 #ifdef DISABLE_LORAMAC_DUTYCYCLE
     LoRaMacTestSetDutyCycleOn(false);
 #endif

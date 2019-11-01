9c618bd01e6f18cedb3d78f492a7372682e488d1
Oleg Artamonov <oleg@unwds.com>
2019-11-01 13:22:58 +0300

    cpu.stm32l1: fix ADC

--- cpu/stm32l1/periph/adc.c
+++ cpu/stm32l1/periph/adc.c
@@ -117,7 +117,7 @@ static void adc_set_sample_time(uint8_t time)
     for (i = 0; i <= 9; i++) {
         reg32 |= (time << (i * 3));
     }
-#if !defined STM32L1XX_MD
+#if defined(CPU_MODEL_STM32L152RE)
     ADC1->SMPR0 = reg32;
 #endif
     ADC1->SMPR1 = reg32;

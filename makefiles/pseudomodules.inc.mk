32a1a422721186b1290bf982f602cb96f9e9f77e
Oleg Artamonov <oleg@unwds.com>
2019-12-05 12:21:12 +0300

    gnrc_netif_shell pseudomodule added

--- makefiles/pseudomodules.inc.mk
+++ makefiles/pseudomodules.inc.mk
@@ -24,6 +24,7 @@ PSEUDOMODULES += gnrc_netdev_default
 PSEUDOMODULES += gnrc_neterr
 PSEUDOMODULES += gnrc_netapi_callbacks
 PSEUDOMODULES += gnrc_netapi_mbox
+PSEUDOMODULES += gnrc_netif_shell
 PSEUDOMODULES += gnrc_pktbuf_cmd
 PSEUDOMODULES += gnrc_sixloenc
 PSEUDOMODULES += gnrc_sixlowpan_border_router_default
